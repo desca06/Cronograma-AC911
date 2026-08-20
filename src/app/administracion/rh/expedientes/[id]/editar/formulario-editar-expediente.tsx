ab759d8cadcc1c6b3338eb76c33ac8d20e599510
Diego Escalante <diegdeleon9@gmail.com>
2026-08-20 11:04:22 -0600

    Cambios visuales

--- src/app/administracion/rh/expedientes/[id]/editar/formulario-editar-expediente.tsx
+++ src/app/administracion/rh/expedientes/[id]/editar/formulario-editar-expediente.tsx
@@ -232,6 +232,7 @@ export function FormularioEditarExpediente({
           }
           className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm text-slate-900 outline-none transition focus:border-blue-500 focus:ring-4 focus:ring-blue-100"
         />
+        
         <input
           id="salarioInicial"
           name="salarioInicial"
@@ -241,6 +242,8 @@ export function FormularioEditarExpediente({
               ? expediente.salarioInicial / 100
               : ""
           }
+          className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm text-slate-900 outline-none transition focus:border-blue-500 focus:ring-4 focus:ring-blue-100"
+
         />
 
         <input
@@ -252,6 +255,8 @@ export function FormularioEditarExpediente({
               ? expediente.salarioActual / 100
               : ""
           }
+          className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm text-slate-900 outline-none transition focus:border-blue-500 focus:ring-4 focus:ring-blue-100"
+
         />
 
         <div>
